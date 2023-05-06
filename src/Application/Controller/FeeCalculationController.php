ac968ed0e5a26db4c87d474c0d8e3312a6b6e38d
Bartosz Grzegorzewski <bartosz.grzegorzewski@daftcode.pl>
2023-05-06 11:58:42 +0200

    Change namespace

---
+++ src/Application/Controller/FeeCalculationController.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class FeeCalculationController extends AbstractController
+{
+
+}
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\FeeCalculator;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
